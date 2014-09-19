f2d466662c0717599daeaf5bffe44b4155d9a050
Stephen Sawchuk <sawchuk@gmail.com>
2014-09-18 21:42:03 -0400

    new

--- components/docs/docs.js
+++ components/docs/docs.js
@@ -1,9 +1,9 @@
 angular
-  .module('gcloud.docs', ['ngRoute', 'hljs'])
-  .config(function($routeProvider) {
+  .module('gcloud.docs', ['ngRoute', 'hljs', 'config'])
+  .config(function($routeProvider, versions) {
     'use strict';
 
-    function filterDocJson($sce) {
+    function filterDocJson($sce, version) {
       // Transform JSON response to remove extraneous objects, such as copyright
       // notices & use strict directives.
       function formatHtml(str) {
@@ -42,7 +42,7 @@ angular
         };
         var a = document.createElement('a');
         return str.replace(regex.see, function(match, module) {
-          a.href = '#/docs/' + module;
+          a.href = '#/docs/' + version + '/' + module;
           a.innerText = module;
           return a.outerHTML;
         });
@@ -67,6 +67,9 @@ angular
             return {
               data: obj,
               name: obj.ctx.name,
+              constructor: obj.tags.some(function(tag) {
+                  return tag.type === 'constructor';
+                }),
               description: $sce.trustAsHtml(
                   formatHtml(detectLinks(detectModules(obj.description.full)))),
               params: obj.tags.filter(function(tag) {
@@ -77,6 +80,7 @@ angular
                       formatHtml(tag.description.replace(/^- /, '')));
                   tag.types = $sce.trustAsHtml(tag.types.reduceRight(
                       reduceModules, []).join(', '));
+                  tag.optional = tag.types.toString().substr(-1) === '=';
                   return tag;
                 }),
               returns: obj.tags.filter(function(tag) {
@@ -95,12 +99,12 @@ angular
             };
           })
           .sort(function(a, b) {
-            return a.name > b.name;
+            return a.constructor ? -1: a.name > b.name;
           });
       };
     }
 
-    function setMethod($location, methodName) {
+    function setMethod($location, methodName, version) {
       return function(methods) {
         var methodExists = methods.some(function(methodObj) {
           return methodName === methodObj.name;
@@ -109,7 +113,7 @@ angular
           methods.singleMethod = methodName;
           return methods;
         } else {
-          $location.path('/docs/' + module + '/' + cl);
+          $location.path('/docs/' + version + '/' + module + '/' + cl);
         }
       };
     }
@@ -121,12 +125,20 @@ angular
 
     $routeProvider
       .when('/docs', {
+        redirectTo: '/docs/' + versions[0]
+      })
+      .when('/docs/history', {
+        controller: 'HistoryCtrl',
+        templateUrl: 'components/docs/docs.html'
+      })
+      .when('/docs/:version', {
         controller: 'DocsCtrl',
         templateUrl: 'components/docs/docs.html',
         resolve: {
-          methods: function($http, $sce) {
-            return $http.get('json/index.json')
-                .then(filterDocJson($sce))
+          methods: function($http, $route, $sce) {
+            var version = $route.current.params.version;
+            return $http.get('json/' + version + '/index.json')
+                .then(filterDocJson($sce, version))
                 .then(function(methods) {
                   // Prevent displaying permalinks.
                   // ** Can remove when PubSub api is documented **
@@ -136,81 +148,95 @@ angular
           }
         }
       })
-      .when('/docs/:module', {
+      .when('/docs/:version/:module', {
         controller: 'DocsCtrl',
         templateUrl: 'components/docs/docs.html',
         resolve: {
           methods: function($http, $route, $sce) {
+            var version = $route.current.params.version;
             var module = $route.current.params.module;
-            return $http.get('json/' + module + '/index.json')
-                .then(filterDocJson($sce));
+            return $http.get('json/' + version + '/' + module + '/index.json')
+                .then(filterDocJson($sce, version));
           }
         }
       })
-      .when('/docs/:module/:class', {
+      .when('/docs/:version/:module/:class', {
         controller: 'DocsCtrl',
         templateUrl: 'components/docs/docs.html',
         resolve: {
           methods: function($q, $http, $route, $sce, $location) {
+            var version = $route.current.params.version;
             var module = $route.current.params.module;
             var cl = $route.current.params.class;
             if (MODULE_TO_CLASSES[module].length > 0) {
               return $http
-                  .get('json/' + module + '/' + cl + '.json')
-                  .then(filterDocJson($sce));
+                  .get('json/' + version + '/' + module + '/' + cl + '.json')
+                  .then(filterDocJson($sce, version));
             } else {
               // This is not a class, this is the name of a method.
               var method = cl;
-              return $http.get('json/' + module + '/index.json')
-                  .then(filterDocJson($sce))
-                  .then(setMethod($location, method));
+              return $http.get('json/' + version + '/' +module + '/index.json')
+                  .then(filterDocJson($sce, version))
+                  .then(setMethod($location, method, version));
             }
           }
         }
       })
-      .when('/docs/:module/:class/:method', {
+      .when('/docs/:version/:module/:class/:method', {
         controller: 'DocsCtrl',
         templateUrl: 'components/docs/docs.html',
         resolve: {
           methods: function($q, $http, $route, $sce, $location) {
+            var version = $route.current.params.version;
             var module = $route.current.params.module;
             var cl = $route.current.params.class;
             var method = $route.current.params.method;
-            return $http.get('json/' + module + '/' + cl + '.json')
-                .then(filterDocJson($sce))
-                .then(setMethod($location, method));
+            return $http.get('json/' + version + '/' + module + '/' + cl + '.json')
+                .then(filterDocJson($sce, version))
+                .then(setMethod($location, method, version));
           }
         }
       });
   })
-  .controller('DocsCtrl', function($location, $scope, $routeParams, methods) {
+
+  .controller('DocsCtrl', function($location, $scope, $routeParams, methods, $http, versions) {
     'use strict';
 
     $scope.isActiveUrl = function(url) {
-      var current = $location.path().replace('/' + methods.singleMethod, '');
-      var link = url
-          .replace(/^#/, '')
-          .replace('/' + methods.singleMethod, '');
-      return current === link;
+      var active = $location.path()
+          .replace('/' + methods.singleMethod, '')
+          .replace('/' + $scope.version, '');
+      url = url.replace('#', '').replace('/' + $scope.version, '');
+      return active === url;
     };
 
     $scope.isActiveDoc = function(doc) {
       return doc.toLowerCase() === $routeParams.module;
     };
 
+    $scope.pageTitle = 'Node.js';
+    $scope.showReference = true;
     $scope.activeUrl = '#' + $location.path();
     $scope.singleMethod = methods.singleMethod;
     $scope.noPermalink = methods.singleMethod || methods.noPermalink;
     $scope.methods = methods;
     $scope.module = $routeParams.module;
+    $scope.version = $routeParams.version;
+    $scope.isLatestVersion = $scope.version == versions[0];
+    $scope.versions = versions;
+    var baseUrl = '#/docs/' + $scope.version;
+    /*
+    TODO(silvano): future versions will introduce new pages, so the list below will have
+    to be generated according to the specific version
+    */
     $scope.pages = [
       {
         title: 'gcloud',
-        url: '#/docs'
+        url: baseUrl
       },
       {
         title: 'Datastore',
-        url: '#/docs/datastore',
+        url: baseUrl + '/datastore',
         pages: [
           {
             title: 'Dataset',
@@ -224,7 +250,13 @@ angular
       },
       {
         title: 'Storage',
-        url: '#/docs/storage'
+        url: baseUrl + '/storage'
       }
     ];
+  })
+
+  .controller('HistoryCtrl', function($scope, versions) {
+    $scope.pageTitle = 'Node.js Docs Versions';
+    $scope.showHistory = true;
+    $scope.versions = versions;
   });
